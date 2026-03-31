9b11ed626ded91190a528d24e4358d37017ccb05
jserfeng <1114550440@qq.com>
2026-03-31 12:05:01 +0800

    fix(core): clarify side effects state APIs

--- crates/node_binding/napi-binding.d.ts
+++ crates/node_binding/napi-binding.d.ts
@@ -2478,6 +2478,11 @@ jsx?: boolean
  * @experimental
  */
 importMetaResolve?: boolean
+/**
+ * Flag top-level functions or exports as side-effect-free for advanced tree shaking.
+ * This option is experimental in Rspack only and subject to change or be removed anytime.
+ * @experimental
+ */
 sideEffectsFree?: Array<string>
 }
 
